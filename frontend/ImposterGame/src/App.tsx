b475df9869a11b6fcb637e80a445824b0dbdb43b
jamespham <jpham07@student.ubc.ca>
2026-02-20 18:41:25 -0700

    Create welcome page

--- frontend/ImposterGame/src/App.tsx
+++ frontend/ImposterGame/src/App.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
+import Welcome from "./pages/Welcome/Welcome.tsx";
+import Info from "./pages/Info/Info.tsx";
+import JoinForm from "./pages/JoinForm/JoinForm.tsx";
+
 function App() {
+  const [isJoinOpen, setIsJoinOpen] = useState(false);
+  const [isInfoOpen, setIsInfoOpen] = useState(false);
+
   return (
     <>
-      <div className="h-screen bg-navy">
-      </div>
+      <Welcome
+        onInfoClick={() => setIsInfoOpen(true)}
+        onJoinClick={() => setIsJoinOpen(true)}
+      />
+      {isJoinOpen && <JoinForm onCancelJoinClick={() => setIsJoinOpen(false)} />}
+      {isInfoOpen && <Info onInfoExitClick={() => setIsInfoOpen(false)} />}
     </>
   )
 }
