696ce9298df75d31fceca549ddf6ffc18d8448db
jamespham <jpham07@student.ubc.ca>
2026-02-21 11:11:54 -0700

    Fix App.tsx

--- frontend/ImposterGame/src/pages/Welcome.tsx
+++ frontend/ImposterGame/src/pages/Welcome.tsx
@@ -2,19 +2,21 @@ import { Github } from "lucide-react";
 import { CircleQuestionMark } from "lucide-react";
 import { Link } from 'react-router-dom';
 
-type WelcomeProps = {
-    onInfoClick: () => void
-    onJoinClick: () => void;
-}
+import { useState } from "react";
 
-export default function Welcome({ onInfoClick, onJoinClick }: WelcomeProps) {
+import Info from "../components/Info.tsx";
+import JoinForm from "../components/JoinForm.tsx";
+
+export default function Welcome() {
+    const [isJoinOpen, setIsJoinOpen] = useState(false);
+    const [isInfoOpen, setIsInfoOpen] = useState(false);
     return (
         <>
             <div className="h-screen bg-gray-950">
                 <div className="flex justify-between">
                     <button
                         type="button"
-                        onClick={onInfoClick}
+                        onClick={() => setIsInfoOpen(true)}
                         className="m-5 p-2 rounded-xl text-gray-200 bg-purple-950 hover:bg-purple-900 transition-colors duration-300 cursor-pointer">
                         <CircleQuestionMark size={24} />
                     </button>
@@ -34,7 +36,7 @@ export default function Welcome({ onInfoClick, onJoinClick }: WelcomeProps) {
                 <div className="flex justify-center">
                     <button
                         type="button"
-                        onClick={onJoinClick}
+                        onClick={() => setIsJoinOpen(true)}
                         className="cursor-pointer w-30 m-2 p-3 mt-10 rounded-xl font-bold text-sm text-gray-200 bg-purple-950 hover:bg-purple-900 transition-colors duration-300">
                         Join Room
                     </button>
@@ -48,6 +50,8 @@ export default function Welcome({ onInfoClick, onJoinClick }: WelcomeProps) {
 
                 </div>
             </div>
+            {isJoinOpen && <JoinForm onCancelJoinClick={() => setIsJoinOpen(false)} />}
+            {isInfoOpen && <Info onInfoExitClick={() => setIsInfoOpen(false)} />}
         </>
     );
 }
