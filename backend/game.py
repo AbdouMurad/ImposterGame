b95047dcacd78f371907a7cc39fd996eb4f643be
Lem-D <lemueljd12@gmail.com>
2026-02-21 23:47:13 -0700

    removed commented categories

--- backend/game.py
+++ backend/game.py
@@ -159,7 +159,7 @@ class Game:
         questions = {
             q["id"]: {
                 "title": q["title"],
-               # "category": q["category"],
+                "category": q["category"],
                 "difficulty": q["difficulty"],
                 "description": q["description"],
                 "examples": q["examples"],
@@ -175,7 +175,7 @@ class Game:
         self.commit("SYSTEM", question["starter_code"])
         self.questionDifficulty = question["difficulty"]
         self.questionTitle = question["title"]
-        #self.questionCategory = question["category"]
+        self.questionCategory = question["category"]
         self.questionDesc = question["description"]
         self.questionExample = question["examples"]
         self.questionStarterCode = question["starter_code"]    
